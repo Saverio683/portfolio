b90844b17a3e6b486a5d68b7a7bc129f6976fac6
Saverio683 <saveriorandazzo50@gmail.com>
2023-09-15 16:10:42 +0200

    removing react-router

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,23 +1,14 @@
-import { lazy, Suspense } from 'react'
-import { Route,  Routes } from 'react-router'
-
 import Header from '../components/header/header.component'
 import ScrollButton from '../components/scroll-button/scroll-button.component'
+import HomePage from '../pages/home/home.page'
 
 import './App.scss'
-
-const HomePage = lazy(() => import('../pages/home/home.page'))
 
 const App = () => {
   return (
     <div className='app'>
       <Header />
-      <Suspense fallback={<span>loading...</span>}>
-        <Routes>
-          <Route path='/' element={<HomePage />} />
-          <Route path='*' element={<span>page not found</span>} />
-        </Routes>
-      </Suspense>
+      <HomePage />
       <ScrollButton />
     </div>
   )
