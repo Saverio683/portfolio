a5e942b6b9235f4e35bc5c433f1da912da7b2692
Saverio683 <saveriorandazzo50@gmail.com>
2023-09-20 19:07:17 +0200

    profile image

--- src/pages/about/about.page.jsx
+++ src/pages/about/about.page.jsx
@@ -36,7 +36,7 @@ const About = () => {
                 <div className='img-ct'>
                     <div className='circle'>
                         <img 
-                            src='https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png' 
+                            src='https://i.ibb.co/bW5wrcB/profile.webp' 
                             alt='profile' 
                         />
                     </div>
