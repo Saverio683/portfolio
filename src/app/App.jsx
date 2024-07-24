9c147978727c6a04ed4472da555b74ef83fb8d6e
anon <anon@pop-os.fritz.box>
2024-07-24 17:01:48 +0200

    adding vercel insights component

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,11 +1,14 @@
 import Header from '../components/header/header.component'
 import HomePage from '../pages/home/home.page'
 
+import { SpeedInsights } from "@vercel/speed-insights/react"
+
 import './App.scss'
 
 const App = () => {
   return (
     <div className='app'>
+      <SpeedInsights /> 
       <Header />
       <HomePage />
     </div>
